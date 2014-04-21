d851ecadc35b4da8e74492fde7eb69613e8ac598
runningtalus <dave@davemay.org>
2014-04-21 10:24:00 -0600

    Added Student constructor

---
+++ README.md
@@ -0,0 +1,49 @@
+Object Oriented Programming (OOP) with JavaScript
+
+Procedural Programming
+	Sequence of commands
+
+Functional Programming
+	Combination of values
+
+Object Oriented Programming (OOP)
+	Combination of Objects
+		Encapsulation
+		Inheritance
+			"is a"
+			"type of"
+		Composition
+			"has a"
+	Combats 
+		Global Variables
+		Procedural
+
+	OOP is a way to handle complexity
+
+	Constructor
+		is a way to define a new type
+		uses the "new" keyword
+		denoted with a capital letter
+		ES5 & ES6 not classes, next version of JS will have classes
+		function used to instantiate an 
+
+
+
+		The Movie constructor can be viewed as a...
+			Template
+			Type
+			We use the Movie constructor to create INSTANCES
+
+		new
+			command to create a new instance of an object using the given constructor
+		
+		this
+			"execution context"
+			specific to the function it is in
+			points to the "host object"
+				new instance
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,3 +1,47 @@
-$(document).on('ready', function() {
-  
-});
+//The Student Class
+
+var Student = function(name){
+	this.name = name;
+	this.grades = [];
+	
+//Prompt's the student for the score, this pushes to grades
+
+	this.takeTest = function(){
+		var score = prompt('What did you receive on the test?');
+		this.grades.push(score);
+	};
+
+//Returns the average of all the student's grades
+
+this.currentGrade = function() {
+	var total = 0;
+	for (var i=0; i<this.grades.length; i++) {
+		total += this.grades[i];
+	}
+	return total/this.grades.length;
+	};
+};
+
+
+var tom = new Student('Tom');
+var sarah = new Student('Sarah');
+
+//tom.takeTest();
+//prompts user to enter their score, prompt score is pushed to grades array
+//entered 86 to the prompt
+
+//tom.grades
+//logs 86
+
+tom.grades.push(95);
+tom.grades.push(85);
+tom.grades.push(100);
+
+sarah.grades.push(100);
+sarah.grades.push(100);
+sarah.grades.push(95);
+
+tom.currentGrade();
+//logs 93.333
+sarah.currentGrade();
+//logs 98.333
